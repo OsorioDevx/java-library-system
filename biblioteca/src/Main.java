253fce2fe8d8f440417d4eb865f5cf23ca99c080
Matheus Henrique Gama Osório <henrique.busn@gmail.com>
2026-02-12 16:42:02 -0400

    :wrench: chore - adicionando imports para utilizar as classes na Main

---
+++ biblioteca/src/Main.java
@@ -0,0 +1,19 @@
+/*Percebi que em projetos maiores, de grandes empresas é mais comum fazer o import explícito das outras classes
+* porém você facilmente pode usar import model.* pra puxar tudo de uma vez*/
+import model.ItemBiblioteca;
+import model.Livro;
+import model.Revista;
+import model.Usuario;
+import service.Biblioteca;
+
+/*importação do scanner pra leitura do usuário*/
+import java.util.Scanner;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ biblioteca/src/model/ItemBiblioteca.java
@@ -0,0 +1,8 @@
+package model;
+/*Classe abstrata*/
+
+public class ItemBiblioteca {
+    public static void main(String[] args) {
+
+    }
+}
